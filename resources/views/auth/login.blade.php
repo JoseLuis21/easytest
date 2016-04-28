df713370437c8c5afed04e8c31b82901b6c0177b
Jose <josephluihs@gmail.com>
2016-04-28 00:42:14 -0300

    "cambio en el login"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
-
+  
 {!! Form::open(array('url' => '/login', 'method' => 'POST', 'class'=> '')) !!}
 <div class="ui one column center aligned grid">
   <div class="column six wide form-holder">
     <h2 class="center aligned header form-head">Ingresar</h2>
     <div class="ui form">
-        @if ($errors->has('email'))           
+        @if ($errors->has('email'))
             <div class="ui message">
               <i class="close icon"></i>
               <div class="header">
@@ -18,7 +17,7 @@
         @endif
       <div class="ui left icon fluid input login">
         {!! Form::text('email', old('email'), array('placeholder'=> 'Email', 'class' => '')); !!}
-        <i class="mail icon"></i>        
+        <i class="mail icon"></i>
       </div>
        @if ($errors->has('password'))
             <div class="ui message">
@@ -30,17 +29,17 @@
         @endif
       <div class="ui left icon fluid input login">
         {!! Form::password('password', ['placeholder' => 'Paswoord']) !!}
-        <i class="privacy icon"></i>       
+        <i class="privacy icon"></i>
       </div>
       <div class="inline field">
         <div class="ui checkbox">
-          {!! Form::checkbox('remember'); !!} 
+          {!! Form::checkbox('remember'); !!}
           <label>Recuerdame</label>
         </div>
       </div>
       <div class="inline field">
          {!! Form::submit('Ingresar a EasyTest', ['class' => 'ui button primary']); !!}
-         {!! link_to('/password/reset', $title = 'Olvidaste tu contraseña?', $attributes = array('class' => 'ui button secondary'), $secure = null); !!}                 
+         {!! link_to('/password/reset', $title = 'Olvidaste tu contraseña?', $attributes = array('class' => 'ui button secondary'), $secure = null); !!}
       </div>
     </div>
   </div>
